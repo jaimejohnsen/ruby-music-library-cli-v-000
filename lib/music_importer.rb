78381ba2de3d11fcf641e45abda310c6545c39a5
Jaime Johnsen <drivetraindiva@gmail.com>
2020-12-17 18:01:06 +0000

    Automatically backed up by Learn

--- lib/music_importer.rb
+++ lib/music_importer.rb
@@ -15,21 +15,4 @@
     end
 end
 
-  #def initialize(path)
-  #  @path = path
-  #end
 
-  #path retrieves the path provided to the MusicImporter object
-  #def path(path = './db/mp3s')
-
-  #end
-
-  #def files
-      #@files || = Dir.glob("#{path}/*.mp3").collect{ |f| f.gsub("#{path}/", "") }
-    #removes the actual path that is attached to filename and collects the filename only
-  #end
-
-#  def import
-#    files.each {|file| Song.new_from_filename(file)}
-#  end
-#end
